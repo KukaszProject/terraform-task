5777654c065ec07fccfecc9c81652470732356e6
Lukasz Kielbasa <hamsingming@gmail.com>
2026-08-04 13:46:04 +0200

    Fixed versions

--- task06/outputs.tf
+++ task06/outputs.tf
@@ -1,7 +1,9 @@
 output "sql_server_fqdn" {
-  value = module.sql.sql_server_fqdn
+  value       = module.sql.sql_server_fqdn
+  description = "The fully qualified domain name of the Azure SQL Server."
 }
 
 output "app_hostname" {
-  value = module.webapp.app_hostname
+  value       = module.webapp.app_hostname
+  description = "The default hostname of the Linux Web App."
 }

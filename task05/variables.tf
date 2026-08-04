02311ef6a1ed059ec27c9a628a583cd9c94148fa
Lukasz Kielbasa <hamsingming@gmail.com>
2026-08-04 11:44:57 +0200

    Added descriptions

--- task05/variables.tf
+++ task05/variables.tf
@@ -3,50 +3,54 @@ variable "resource_groups" {
     name     = string
     location = string
   }))
-  description = "A map of resource group objects containing their names and locations."
+  description = "A map of resource group objects containing their name and location."
 }
 
 variable "app_service_plans" {
   type = map(object({
     name         = string
-    rg_key       = string
-    sku          = string
     worker_count = number
+    sku          = string
+    rg_key       = string
   }))
-  description = "A map of App Service Plan configurations including SKU and worker instances."
+  description = "A map defining the App Service Plans and their scaling configurations."
 }
 
 variable "app_services" {
   type = map(object({
     name    = string
-    rg_key  = string
     asp_key = string
+    rg_key  = string
   }))
-  description = "A map of Windows Web Apps and their respective IP access restriction security rules."
-}
-
-variable "verification_agent_ip" {
-  type        = string
-  description = "Verification agent IP."
+  description = "A map defining the Windows Web Apps and their target service plans."
 }
 
 variable "tags" {
   type        = map(string)
-  description = "A map of tags applied across all deployed Azure resources."
+  description = "A mapping of standard tags to assign to all resources."
+}
+
+variable "verification_agent_ip" {
+  type        = string
+  description = "The IP address of the verification agent for access restrictions."
 }
 
 variable "ip_rule_name" {
-  type = string
+  type        = string
+  description = "The name of the access restriction rule allowing the verification agent IP."
 }
 
 variable "tm_rule_name" {
-  type = string
+  type        = string
+  description = "The name of the access restriction rule allowing Azure Traffic Manager."
 }
 
 variable "tm_profile_name" {
-  type = string
+  type        = string
+  description = "The name of the Azure Traffic Manager profile."
 }
 
 variable "tm_routing_method" {
-  type = string
+  type        = string
+  description = "The routing method used by the Azure Traffic Manager profile."
 }

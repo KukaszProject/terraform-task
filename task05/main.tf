d3f125f437c229cb2eaba2940bb82b4276791d1c
Lukasz Kielbasa <hamsingming@gmail.com>
2026-08-04 10:07:10 +0200

    Removed unnecessary imports

--- task05/main.tf
+++ task05/main.tf
@@ -58,21 +58,6 @@ module "traffic_manager" {
 }
 
 import {
-  to = module.resource_groups["rg1"].azurerm_resource_group.rg
-  id = "/subscriptions/fe7f3f41-b0fa-47c8-84f7-d5e8ae76595f/resourceGroups/cmaz-ac643e5v-mod5-rg-01"
-}
-
-import {
-  to = module.resource_groups["rg2"].azurerm_resource_group.rg
-  id = "/subscriptions/fe7f3f41-b0fa-47c8-84f7-d5e8ae76595f/resourceGroups/cmaz-ac643e5v-mod5-rg-02"
-}
-
-import {
-  to = module.resource_groups["rg3"].azurerm_resource_group.rg
-  id = "/subscriptions/fe7f3f41-b0fa-47c8-84f7-d5e8ae76595f/resourceGroups/cmaz-ac643e5v-mod5-rg-03"
-}
-
-import {
   to = module.app_service_plans["asp1"].azurerm_service_plan.asp
   id = "/subscriptions/fe7f3f41-b0fa-47c8-84f7-d5e8ae76595f/resourceGroups/cmaz-ac643e5v-mod5-rg-01/providers/Microsoft.Web/serverFarms/cmaz-ac643e5v-mod5-asp-01"
 }

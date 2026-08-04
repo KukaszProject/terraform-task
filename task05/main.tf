1b3473c982b5e848fd38af8591a4a4307840bae3
Lukasz Kielbasa <hamsingming@gmail.com>
2026-08-04 09:44:17 +0200

    Added imports

--- task05/main.tf
+++ task05/main.tf
@@ -74,12 +74,12 @@ import {
 
 import {
   to = module.app_service_plans["asp1"].azurerm_service_plan.asp
-  id = "/subscriptions/fe7f3f41-b0fa-47c8-84f7-d5e8ae76595f/resourceGroups/cmaz-ac643e5v-mod5-rg-01/providers/Microsoft.Web/serverfarms/cmaz-ac643e5v-mod5-asp-01"
+  id = "/subscriptions/fe7f3f41-b0fa-47c8-84f7-d5e8ae76595f/resourceGroups/cmaz-ac643e5v-mod5-rg-01/providers/Microsoft.Web/serverFarms/cmaz-ac643e5v-mod5-asp-01"
 }
 
 import {
   to = module.app_service_plans["asp2"].azurerm_service_plan.asp
-  id = "/subscriptions/fe7f3f41-b0fa-47c8-84f7-d5e8ae76595f/resourceGroups/cmaz-ac643e5v-mod5-rg-02/providers/Microsoft.Web/serverfarms/cmaz-ac643e5v-mod5-asp-02"
+  id = "/subscriptions/fe7f3f41-b0fa-47c8-84f7-d5e8ae76595f/resourceGroups/cmaz-ac643e5v-mod5-rg-02/providers/Microsoft.Web/serverFarms/cmaz-ac643e5v-mod5-asp-02"
 }
 
 import {

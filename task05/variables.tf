8f7cc9e9673bd701bf34fb0ffcb1647d32bf48a0
Lukasz Kielbasa <hamsingming@gmail.com>
2026-08-04 09:21:34 +0200

    Added descriptions to vars

--- task05/variables.tf
+++ task05/variables.tf
@@ -1,21 +1,23 @@
 variable "resource_groups" {
-  type = map(object({
+  type        = map(object({
     name     = string
     location = string
   }))
+  description = "A map of resource group objects containing their names and locations."
 }
 
 variable "app_service_plans" {
-  type = map(object({
+  type        = map(object({
     name         = string
     rg_key       = string
     sku_name     = string
     worker_count = number
   }))
+  description = "A map of App Service Plan configurations including SKU and worker instances."
 }
 
 variable "app_services" {
-  type = map(object({
+  type        = map(object({
     name    = string
     rg_key  = string
     asp_key = string
@@ -27,16 +29,19 @@ variable "app_services" {
       priority    = number
     }))
   }))
+  description = "A map of Windows Web Apps and their respective IP access restriction security rules."
 }
 
 variable "traffic_manager" {
-  type = object({
+  type        = object({
     name           = string
     rg_key         = string
     routing_method = string
   })
+  description = "Configuration settings for the Azure Traffic Manager profile and routing method."
 }
 
 variable "tags" {
-  type = map(string)
+  type        = map(string)
+  description = "A map of tags applied across all deployed Azure resources."
 }

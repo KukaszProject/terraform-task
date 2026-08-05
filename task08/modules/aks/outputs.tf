33f37baf263985ed2b75fdbd1c6fe48e5782080e
Lukasz Kielbasa <hamsingming@gmail.com>
2026-08-05 15:41:17 +0200

    Fixed plan

--- task08/modules/aks/outputs.tf
+++ task08/modules/aks/outputs.tf
@@ -8,3 +8,27 @@ output "tenant_id" {
   description = "The Tenant ID of the AKS cluster identity."
   value       = azurerm_kubernetes_cluster.aks.identity[0].tenant_id
 }
+
+output "host" {
+  description = "The Kubernetes cluster server host."
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  sensitive   = true
+}
+
+output "client_certificate" {
+  description = "Base64 encoded public certificate used by clients to authenticate."
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  sensitive   = true
+}
+
+output "client_key" {
+  description = "Base64 encoded private key used by clients to authenticate."
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+  sensitive   = true
+}
+
+output "cluster_ca_certificate" {
+  description = "Base64 encoded public CA certificate used to validate the cluster."
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+  sensitive   = true
+}

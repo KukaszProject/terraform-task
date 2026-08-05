052324e1ecfa527e7c38edf41f13995055aeb1c9
Lukasz Kielbasa <hamsingming@gmail.com>
2026-08-05 10:06:54 +0200

    Removed locals

--- task07/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  # Define any local values here. 
-  # For example, standard tags to apply to all resources:
-  tags = {
-    environment = "task07-cdn-frontdoor"
-    managed_by  = "terraform"
-  }
-}
